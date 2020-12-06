70681069447112cab82f6637aa7a2c5f79c21b46
Shakhawat <shsourov2000@gmail.com>
2020-12-06 12:27:27 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+   
+    $('.owl-carousel').owlCarousel({
+        margin: 15,
+        responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        968: {
+            items: 3
+        },
+    }
+                                   
+    });
+    
+});
